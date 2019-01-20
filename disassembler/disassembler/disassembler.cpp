3a5f2d5d3bc5d16949a64e29c8bfc9e3a664643a
Shreeshaa Goyal <shreeshaa3@gmail.com>
2019-01-19 22:04:41 -0800

    Functional movq and OPq instructions

--- disassembler/disassembler/disassembler.cpp
+++ disassembler/disassembler/disassembler.cpp
@@ -29,20 +29,21 @@ void movq(unsigned char c, int *res, FILE *out, FILE *machineCode);
 void irmovq(unsigned char c, int *res, FILE *out);
 void rmmovq(unsigned char c, int *res, FILE *out);
 void mrmovq(unsigned char c, int *res, FILE *out);
-void OPq(unsigned char c, int *res, FILE *out);
+void OPq(unsigned char c, int *res, FILE *out, FILE *machineCode);
 void jXX(unsigned char c, int *es, FILE *out);
 void call(unsigned char c, int *res, FILE *out);
 void ret(unsigned char c, int *res, FILE *out);
 void pushq(unsigned char c, int *res, FILE *out);
 void popq(unsigned char c, int *res, FILE *out);
-void condition(unsigned char c, int *res, FILE *out);
+const char* condition(unsigned char ifun);
+const char* reg(unsigned char reg);
 
 int main()
 {
 	FILE *machineCode, *outputFile;
 
 	unsigned int argc = 3;
-	const char *InputFilename = "nop-ret.mem";
+	const char *InputFilename = "cmovge-subq.mem";
 	const char *OutFilename = "";
 	
 	unsigned long startingOffset = 0;
@@ -184,7 +185,7 @@ int printInstruction(FILE *machineCode, FILE *out)
 				mrmovq(c, &res, out);
 				break;
 			case 0x6:
-				OPq(c, &res, out);
+				OPq(c, &res, out, machineCode);
 				break;
 			case 0x7:
 				jXX(c, &res, out);
@@ -212,141 +213,222 @@ int printInstruction(FILE *machineCode, FILE *out)
 // INSTRUCTION FUNCTIONS. MAKE THESE PRIVATE
 void halt(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "halt\n");
 }
 
 void nop(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "nop\n");
 }
 
 void movq(unsigned char c, int *res, FILE *out, FILE *machineCode)
 {
-	if (!(c % 10))
-	{
-		fprintf(out, "rrmovq\n");
-	}
-	else if ((0 > (c % 10)) && ((c % 10) <= 6))
-	{
-		fprintf(out, "cmov");
-		condition(c, res, out);
-	}
-
+	unsigned char ifun = c % 16;
 	unsigned char registers = fgetc(machineCode);
-	unsigned char firstRegister = registers >> 4;
-	unsigned char secondRegister = registers % 10;
+	unsigned char firstRegister;
+	unsigned char secondRegister;
 
-	if ((0 >= firstRegister) && (firstRegister <= 0xE))
+	// check validity of instruction
+	if ((0 <= ifun) && (ifun <= 6))
 	{
-		printRegister(firstRegister, out);
+		firstRegister = registers >> 4;
+		secondRegister = registers % 16;
+
+		if ((0 <= firstRegister)
+			&& (firstRegister <= 0xE)
+			&& (0 <= secondRegister)
+			&& (secondRegister <= 0xE)
+			&& (*res > 0))
+		{
+			*res = 1;
+		}
+		else *res = -1;
+	}
+	else *res = -1;
+
+	// print instruction
+	if (*res > 0)
+	{
+		if (!ifun)
+		{
+			fprintf(out, "rrmovq");
+		}
+		else if (ifun)
+		{
+			fprintf(out, "cmov");
+			condition(c % 16);
+		}
+
+		fprintf(out, "%s %s, %s\n", condition(ifun), reg(firstRegister), reg(secondRegister));
 	}
 }
 
 void irmovq(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "irmovq\n");
 }
 
 void rmmovq(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "rmmovq\n");
 }
 
 void mrmovq(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "mrmovq\n");
 }
 
-void OPq(unsigned char c, int *res, FILE *out)
+void OPq(unsigned char c, int *res, FILE *out, FILE *machineCode)
 {
-	switch (c % 10)
+	unsigned char ifun = c % 16;
+	unsigned char registers = fgetc(machineCode);
+	unsigned char firstRegister;
+	unsigned char secondRegister;
+
+	// check validity of instruction
+	if ((0 <= ifun) && (ifun <= 6))
 	{
+		firstRegister = registers >> 4;
+		secondRegister = registers % 16;
+
+		if ((0 <= firstRegister)
+			&& (firstRegister <= 0xE)
+			&& (0 <= secondRegister)
+			&& (secondRegister <= 0xE)
+			&& (*res > 0))
+		{
+			*res = 1;
+		}
+		else *res = -1;
+	}
+	else *res = -1;
+
+	// print instruction
+	if (*res > 0)
+	{
+		switch (ifun)
+		{
 		case 0:
-			fprintf(out, "addq\n");
+			fprintf(out, "addq");
 			break;
 		case 1:
-			fprintf(out, "subq\n");
+			fprintf(out, "subq");
 			break;
 		case 2:
-			fprintf(out, "andq\n");
+			fprintf(out, "andq");
 			break;
 		case 3:
-			fprintf(out, "xorq\n");
+			fprintf(out, "xorq");
 			break;
 		case 4:
-			fprintf(out, "mulq\n");
+			fprintf(out, "mulq");
 			break;
 		case 5:
-			fprintf(out, "divq\n");
+			fprintf(out, "divq");
 			break;
 		case 6:
-			fprintf(out, "modq\n");
+			fprintf(out, "modq");
 			break;
 		default:
 			*res = -1;
+		}
+
+		fprintf(out, " %s, %s\n", reg(firstRegister), reg(secondRegister));
 	}
 }
 
 void jXX(unsigned char c, int *res, FILE *out)
 {
-	fprintf(out, "j");
-	condition(c, res, out);
+	unsigned char ifun = c % 16;
+	if ((0 <= ifun) && (ifun <= 6))
+	{
+		fprintf(out, "j%s", condition(ifun));
+	}
 }
 
 void call(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "call\n");
 }
 
 void ret(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "ret\n");
 }
 
 void pushq(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "pushq\n");
 }
 
 void popq(unsigned char c, int *res, FILE *out)
 {
-	*res = !(c % 10) ? 1 : -1;
+	*res = !(c % 16) ? 1 : -1;
 	fprintf(out, "popq\n");
 }
 
-void condition(unsigned char c, int *res, FILE *out)
+const char* condition(unsigned char ifun)
 {
-	unsigned char ifun = c % 10;
-	*res = ((0 <= ifun) && (ifun <= 6)) ? 1 : -1;
 	switch (ifun)
 	{
+		case 0:
+			return "";
 		case 1:
-			fprintf(out, "le\n");
-			break;
+			return "le";
 		case 2:
-			fprintf(out, "l\n");
-			break;
+			return "l";
 		case 3:
-			fprintf(out, "e\n");
-			break;
+			return "e";
 		case 4:
-			fprintf(out, "ne\n");
-			break;
+			return "ne";
 		case 5:
-			fprintf(out, "ge\n");
-			break;
+			return "ge";
 		case 6:
-			fprintf(out, "g\n");
-			break;
-		default:
-			*res = -1;
+			return "g";
+	}
+}
+
+const char* reg(unsigned char reg)
+{
+	switch (reg)
+	{
+		case 0x0:
+			return "%rax";
+		case 0x1:
+			return "%rcx";
+		case 0x2:
+			return "%rdx";
+		case 0x3:
+			return "%rbx";
+		case 0x4:
+			return "%rsp";
+		case 0x5:
+			return "%rbp";
+		case 0x6:
+			return "%rsi";
+		case 0x7:
+			return "%rdi";
+		case 0x8:
+			return "%r8";
+		case 0x9:
+			return "%r9";
+		case 0xA:
+			return "%r10";
+		case 0xB:
+			return "%r11";
+		case 0xC:
+			return "%r12";
+		case 0xD:
+			return "%r13";
+		case 0xE:
+			return "%r14";
 	}
 }
